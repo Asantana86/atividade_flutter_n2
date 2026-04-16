820d35233835a8c60c41d847e18bd5b2fa8d9f7d
airton <airtonfilhonasc@gmail.com>
2026-04-16 20:51:38 -0300

    adicionado routes, widget, app_logo

---
+++ lib/app/shared/app_routes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'modules/splash/presentation/pages/splash_page.dart';
+import 'modules/auth/presentation/pages/login_page.dart';
+
+class AppRoutes {
+  static const login = '/auth/login';
+
+  static Map<String, WidgetBuilder> get routes => {
+    splash: (_) => const SplashPage(),
+    login: (_) => const LoginPage(),
+  };
+}
